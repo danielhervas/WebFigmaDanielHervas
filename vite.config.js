ce8c8d7023262f43e5bd031e8f88c222fe4ce121
Dani <alumno.393791@ies-azarquiel.es>
2024-12-02 13:13:17 +0100

    Primer commit de mi proyecto

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    root: "./src", // Cambia la raíz del proyecto a "src"
+    outDir: "../docs", // Cambia el directorio de salida
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"), // Página principal
+        about: resolve(__dirname, "src/iniciosesion.html"), // Otra página
+        // Añade más entradas si es necesario
+      },
+    },
+  },
+  base: "./", // Opcional: rutas relativas para mayor compatibilidad
+});
