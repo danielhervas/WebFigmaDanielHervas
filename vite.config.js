1203e1e5ac45afd46c2563e1d345f4700b94ca9f
Dani <alumno.393791@ies-azarquiel.es>
2024-12-02 18:37:52 +0100

    v1

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+  
   build: {
     root: "./src", // Cambia la raíz del proyecto a "src"
-    outDir: "../docs", // Cambia el directorio de salida
+    outDir: "docs", // Cambia el directorio de salida
     rollupOptions: {
       input: {
         main: resolve(__dirname, "./src/index.html"), // Página principal
